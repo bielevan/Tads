e66c00e0fc55d83daa547d5361731a6438bf7ae1
lllgabrielll <evangelista.gr@live.com>
2016-10-26 08:49:15 -0200

    Atualizacao

---
+++ Banco/banco_de_memoria.h
@@ -0,0 +1,21 @@
+#define TRUE 1
+#define FALSE 0
+#define MAX 100
+
+typedef int elem;
+
+typedef struct no
+{
+	elem e;
+}NO;
+
+typedef struct no
+{
+	
+}NO;
+
+typedef struct banco
+{
+	int proximo;
+	int vet[MAX];
+}B;
